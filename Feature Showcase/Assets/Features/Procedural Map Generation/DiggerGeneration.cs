05d3782258329c8ecb5092b68c41946303e8d147
Huntrt <huntrt.business@outlook.com>
2022-06-16 00:33:13 +0700

    Able to bypass plot if an miner got stuck

--- Feature Showcase/Assets/Features/Procedural Map Generation/DiggerGeneration.cs
+++ Feature Showcase/Assets/Features/Procedural Map Generation/DiggerGeneration.cs
@@ -7,7 +7,7 @@ using System;
 public class DiggerGeneration : MonoBehaviour
 {
 	public bool autoGenerate; public float autoDelay;
-	[Header("> Setting")]
+	[Header("- Settings -")]
 	[Tooltip("The amount of plot will need to dig")]
 	public int amount;
 	[Tooltip("The position to begin dig")]
@@ -19,7 +19,7 @@ public class DiggerGeneration : MonoBehaviour
 	[Tooltip("The constraint for the miner when dig")]
 	public DigRequirement digRequirement;
 	public Builder builder;
-	[Header("> Data List")]
+	[Header("- Data List -")]
 	public List<PlotData> plots = new List<PlotData>();
 	public List<PlotData> miners = new List<PlotData>();
 	public List<DraftData> drafts = new List<DraftData>();
@@ -50,37 +50,39 @@ public class DiggerGeneration : MonoBehaviour
 		public bool autoScale;
 		public Draft draft; [Serializable] public class Draft
 		{
-			public GameObject grouper;
 			public GameObject prefab;
 			public float size;
-			public Color digged, miner, stuck, over;
+			public Color digged, miner, stuck, bypassed, over;
+			public enum Colors {digged, miner, stuck, bypassed, over};
+			[HideInInspector] public GameObject grouper;
 		}
 		public Floor floor; [Serializable] public class Floor
 		{
-			public GameObject grouper;
 			public GameObject prefab;
 			public float size;
 			public Color color;
+			[HideInInspector] public GameObject grouper;
 		}
 		public Bridge bridge; [Serializable] public class Bridge
 		{
-			public GameObject grouper;
 			public GameObject prefab;
 			public float width, length;
 			public Color color;
+			[HideInInspector] public GameObject grouper;
 		}
 		public Wall wall; [Serializable] public class Wall
 		{
-			public GameObject grouper;
 			public GameObject prefab;
 			public float thick, length;
 			public Color color;
+			[HideInInspector] public GameObject grouper;
 		}
 	}
 	[Serializable] public class PlotData
 	{
 		public int index, digCount;
 		public Vector2 coordinate, position;
+		public List<int> availableDirection = new List<int>{0,1,2,3};
 		public NeighborData[] neighbors = new NeighborData[4];
 	}
 	[Serializable] public class NeighborData
@@ -111,8 +113,13 @@ public class DiggerGeneration : MonoBehaviour
 	}
 #endregion
 
-	//Begin dig when pressed space
-	void Update() {if(Input.GetKey(KeyCode.Space)) Dig();}
+	void Update() 
+	{
+		//% DIG when pressed SPACE
+		if(Input.GetKey(KeyCode.Space)) Dig();
+		//% LOAD THIS SCENE when pressed R
+		if(Input.GetKeyDown(KeyCode.R)) UnityEngine.SceneManagement.SceneManager.LoadScene(3, UnityEngine.SceneManagement.LoadSceneMode.Single);
+	}
 
 	public void Dig(bool overwrite = false)
 	{
@@ -125,12 +132,12 @@ public class DiggerGeneration : MonoBehaviour
 		//Are now generating
 		generating = true;
 		//Create an plot at 0,0 then set it position at start position
-		CreatePlot(new Vector2(0,0), startPosition);
-		//Begin digging at the first plot
+		CreatePlot(0, new Vector2(0,0), startPosition);
+		//Begin digging at that first plot
 		StartCoroutine(Digging(plots[0], plots[0]));
 	}
 
-	IEnumerator Digging(PlotData miner, PlotData digged, int forceDirection = -1)
+	IEnumerator Digging(PlotData miner, PlotData digged)
 	{
 		//This room are now the current leader
 		miners.Add(miner);
@@ -138,20 +145,8 @@ public class DiggerGeneration : MonoBehaviour
 		Drafting(miner, digged);
 		//Wait for an frame
 		yield return null;
-		//Set the result of each direction
-		bool[] result = DirectionResult(forceDirection);
-		//List of available direction
-		List<int> aDir = new List<int>{0,1,2,3};
-		//Go through all 4 direction
-		for (int d = 0; d < 4; d++)
-		{
-			//Randomly get the available direction gonna use
-			int use = UnityEngine.Random.Range(0, aDir.Count);
-			//Try to dig at that available direction with it result
-			TryToDig(miner, aDir[use], result[use]);
-			//This used direction are no longer available
-			aDir.RemoveAt(use);
-		}
+		//Begin decide direction to dig at this miner
+		DecideDirectionToDig(miner);
 		//Remove this miner after try to dig
 		miners.Remove(miner);
 		//Begin check the digging process if haven't dig enough plot
@@ -162,63 +157,94 @@ public class DiggerGeneration : MonoBehaviour
 
 	void TryToDig(PlotData miner, int dir, bool allow)
 	{
-		//Don't dig if has reach max plot amount or not allow
-		if(plots.Count >= amount || !allow) return;
+		//? Checking and prepare the digging direction
 		//Get the vector of this current direction
 		Vector2 dirVector = DirectionIndexToVector(dir);
-		//Get the coordinate in this direction
-		Vector2 dirCoord = miner.coordinate + dirVector;
+		//Get the next coordinate at miner coord increase with direction vector
+		Vector2 nextCoord = miner.coordinate + dirVector;
 		//Find the next plot at direction coordinate
-		PlotData nextPlot = FindPlotAtCoordinate(dirCoord);
-		//If there is already next plot or has reached maximum dig amount
-		if(nextPlot != null || miner.digCount >= digRequirement.maximum)
-		{
-			//Don't dig
-			return;
-		}
+		PlotData nextPlot = FindPlotAtCoordinate(nextCoord);
+		//If this direction haven't got empty neighbor then create one
+		if(miner.neighbors[dir] == null) miner.neighbors[dir] = new NeighborData();
 
-		//? Dig the new plot
-		//Create an new digging plot
-		PlotData newDig = new PlotData();
-		//The dig index are current room count
-		newDig.index = plots.Count;
-		//Set dig coordinate at direction coordinates
-		newDig.coordinate = dirCoord;
-		//Save the spacing and increase the spacing with floor size if using auto scale
-		float spaced = builder.spacing; if(builder.autoScale) {spaced += MasterScaling("floor");}
-		//Set dig position at miner position increase with vector direction that multiply with spaced
-		newDig.position = miner.position + (dirVector * spaced);
-		//Add the digged plot to list
-		plots.Add(newDig);
-		//Counting this dig of miner
-		miner.digCount++;
-		//Begin dig again at newly digged plot
-		StartCoroutine(Digging(newDig, miner));
+		//? Are able to dig in this direction
+		//STOP dig and this direction are no longer available when next plot have exist
+		if(nextPlot != null) {SetDirectionUnavailable(dir, miner); return;}
+		//STOP dig if has reach max plot amount or not allow
+		if(plots.Count >= amount || !allow) {return;}
+		//STOP dig if miner has dig over the maximum allow
+		if(miner.digCount >= digRequirement.maximum) {return;}
+
+		//? Dig an new plot for miner at direction in direction vector with next corrdinate
+		DigPlot(miner, dir, dirVector, nextCoord);
 	}
 
-	void CheckingDigProcess(PlotData miner)
+	void DecideDirectionToDig(PlotData miner)
 	{
-		//Has this miner retry
-		bool retry = true;
-		//If there no miner left or this miner haven't dig the bare minimum 
-		if(miners.Count == 0 || miner.digCount < digRequirement.minimum)
+		//Set the result of each direction
+		bool[] result = DirectionResult();
+		//Create an temporary the list of available direction
+		List<int> aDir = new List<int>(miner.availableDirection);
+		//Go through all 4 direction when there still available direction
+		if(miner.availableDirection.Count > 0) for (int d = 0; d < 4; d++)
 		{
-			//Retry again at the miner
-			StartCoroutine(Digging(miner, miner)); retry = true;
+			//Exit if out of temporary direction
+			if(aDir.Count == 0) {break;}
+			//Randomly get the available direction gonna use
+			int use = UnityEngine.Random.Range(0, aDir.Count);
+			//Try to dig at that available direction with it result
+			TryToDig(miner, aDir[use], result[use]);
+			//Remove this direction from temporary
+			aDir.Remove(use);
 		}
-		//If there still miner left while this miner haven't dig anything and is not retry
-		if(!retry && miner.digCount == 0)
+		//If there are no available direction and this is the only miner
+		else if(miners.Count <= 1)
 		{
-			//% Do something when miner are not needed
+			//Will continuous run until has direction to bypass
+			int bypassDirection = -1; while (bypassDirection == -1)
+			{
+				//Refresh direction result for bypass
+				result = DirectionResult();
+				//Randomly get an choose an direction index
+				int choosed = UnityEngine.Random.Range(0,4);
+				//If the result in choosed direction are true then bypass in that direction
+				if(result[choosed] == true) bypassDirection = choosed;
+			}
+			//Change the draft color at miner index to stuck
+			ChangeDraftColor(miner.index, Builder.Draft.Colors.stuck);
+			//Try to bypass at miner in bypass direction has get
+			TryToBypass(miner, bypassDirection);
 		}
 	}
 
-	bool[] DirectionResult(int forceDirection = -1)
+	void TryToBypass(PlotData bypasser, int direction)
+	{
+		//Get the vector of given direction
+		Vector2 dirVector = DirectionIndexToVector(direction);
+		//Get the next coordinate at bypasser coord increase with direction vector
+		Vector2 nextCoord = bypasser.coordinate + dirVector;
+		//Get the plot data to attempt at coordinate from bypasser increase with vector direction
+		PlotData attempt = FindPlotAtCoordinate(bypasser.coordinate + dirVector);
+		//If there still plot at the attempt
+		if(attempt != null) 
+		{
+			//Change the draft color at attempt index to bypassed
+			ChangeDraftColor(attempt.index, Builder.Draft.Colors.bypassed);
+			//Try to bypass that attempt in the same direction
+			TryToBypass(attempt, direction);
+		}
+		//If there no longer plot at attempt
+		else
+		{
+			//Dig an new plot for bypasser at direction in direction vector with next corrdinate
+			DigPlot(bypasser, direction, dirVector, nextCoord);
+		}
+	}
+
+	bool[] DirectionResult()
 	{
 		//The result for each direction
 		bool[] result = new bool[4];
-		//Set the result of force direction to true if has any then return immediately
-		if(forceDirection != -1) {result[forceDirection] = true; return result;}
 		//Go through all the result need to randomize
 		for (int r = 0; r < result.Length; r++)
 		{
@@ -243,12 +269,56 @@ public class DiggerGeneration : MonoBehaviour
 		return result;
 	}
 
-	PlotData CreatePlot(Vector2 coord, Vector2 pos)
+	void CheckingDigProcess(PlotData miner)
+	{
+		//Has this miner retry
+		bool retry = false;
+		//If there no miner left or this miner haven't dig the bare minimum 
+		if(miners.Count == 0 || miner.digCount < digRequirement.minimum)
+		{
+			//Retry again at this miner
+			StartCoroutine(Digging(miner, miner)); retry = true;
+		}
+		//If this miner haven't dig anything and is not retrying
+		if(!retry && miner.digCount == 0)
+		{
+			//Change the draft color at miner index to over
+			ChangeDraftColor(miner.index, Builder.Draft.Colors.over);
+		}
+	}
+
+	void SetDirectionUnavailable(int dir, PlotData miner) 
+	{
+		//Remove the requested direction from availability of miner if haven't
+		if(miner.availableDirection.Contains(dir)) miner.availableDirection.Remove(dir);
+	}
+
+	void DigPlot(PlotData miner, int dir, Vector2 dirVector, Vector2 nextCoord)
+	{
+		//Save the spacing and increase the spacing with floor size if using auto scale
+		float spaced = builder.spacing; if(builder.autoScale) {spaced += MasterScaling("floor");}
+		//Get the next position by increase miner position with direction got multiply with spaced
+		Vector2 nextPos = miner.position + (dirVector * spaced);
+		//Create an new digged plot with index of plot count at direction coordinate and next position
+		PlotData newDig = CreatePlot(plots.Count, nextCoord, nextPos);
+		//Counting this dig of miner
+		miner.digCount++;
+		//The neighbor in this direction of miner got dig by it
+		miner.neighbors[dir].digByThis = true;
+		//This direction of miner are no longer available
+		SetDirectionUnavailable(dir, miner);
+		///Begin dig again at that newly digged plot
+		StartCoroutine(Digging(newDig, miner));
+	}
+
+	PlotData CreatePlot(int index, Vector2 coord, Vector2 pos)
 	{
 		//Create an new empty plot data
 		PlotData newPlot = new PlotData();
-		//Assign coordinate and position given
-		newPlot.coordinate = coord; newPlot.position = pos;
+		//@ Assign the new plot info as given
+		newPlot.index = index; 
+		newPlot.coordinate = coord; 
+		newPlot.position = pos;
 		//Add the new plot to list and return it
 		plots.Add(newPlot); return newPlot;
 	}
@@ -331,20 +401,40 @@ public class DiggerGeneration : MonoBehaviour
 
 	void Drafting(PlotData miner, PlotData digged)
 	{
+		//Only need to create new draft if some plots haven't get draft
+		if(drafts.Count >= plots.Count) {return;}
 		//Setup an empty new draft
 		DraftData newDraft = new DraftData();
 		//Save the miner index to draft's plot index
 		newDraft.plotIndex = miner.index;
 		//Instantiate an draft prefab at miner position then save it to data
 		newDraft.obj = Instantiate(builder.draft.prefab, miner.position, Quaternion.identity);
-		//Save the new draft object's sprite renderer to data
-		newDraft.renderer = newDraft.obj.GetComponent<SpriteRenderer>();
 		//Set the draft object scale as the master scaled of the draft scale
 		newDraft.obj.transform.localScale = new Vector2(MasterScaling("draft"), MasterScaling("draft"));
 		//Group the draft parent
 		newDraft.obj.transform.SetParent(builder.draft.grouper.transform);
+		//Add plot index to the draft object name
+		newDraft.obj.name = "Draft of " + newDraft.plotIndex;
+		//Save the new draft object's sprite renderer to data
+		newDraft.renderer = newDraft.obj.GetComponent<SpriteRenderer>();
 		//Add new draft data to list
 		drafts.Add(newDraft);
+		//Change the draft color at miner index to miner color
+		ChangeDraftColor(miner.index, Builder.Draft.Colors.miner);
+		//Change the draft color at digged index to digged color
+		ChangeDraftColor(digged.index, Builder.Draft.Colors.digged);
+	}
+
+	void ChangeDraftColor(int index, Builder.Draft.Colors color)
+	{
+		//Get the sprite render of draft at given index
+		SpriteRenderer renderer = drafts[index].renderer;
+		//@ Set that draft color color according to given string
+		if(color == Builder.Draft.Colors.digged)   {renderer.color = builder.draft.digged;   return;}
+		if(color == Builder.Draft.Colors.miner)    {renderer.color = builder.draft.miner;    return;}
+		if(color == Builder.Draft.Colors.stuck)    {renderer.color = builder.draft.stuck;    return;}
+		if(color == Builder.Draft.Colors.bypassed) {renderer.color = builder.draft.bypassed; return;}
+		if(color == Builder.Draft.Colors.over)     {renderer.color = builder.draft.over;     return;}
 	}
 #endregion
 }
